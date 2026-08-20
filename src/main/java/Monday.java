e1eca91c1737be7d4ef6815973470047432c05c4
Jonathan Neo <jonathanntc04@gmail.com>
2026-08-20 15:01:09 +0800

    Added command echoing and bye handling

--- src/main/java/Monday.java
+++ src/main/java/Monday.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 public class Monday {
     public static void main(String[] args) {
-        String banner = " __  __   ___   _   _  ____      _    __   __\n"
+        String banner =
+                "____________________________________________________________\n"
+                + " __  __   ___   _   _  ____      _    __   __\n"
                 + "|  \\/  | / _ \\ | \\ | ||  _ \\    / \\   \\ \\ / /\n"
                 + "| |\\/| || | | ||  \\| || | | |  / _ \\   \\ V / \n"
                 + "| |  | || |_| || |\\  || |_| | / ___ \\   | |  \n"
-                + "|_|  |_| \\___/ |_| \\_||____/ /_/   \\_\\  |_|  \n";
+                + "|_|  |_| \\___/ |_| \\_||____/ /_/   \\_\\  |_|  \n"
+                + "Hello! My name is Monday.\n"
+                + "How can I help you today?\n"
+                + "____________________________________________________________";
         System.out.println(banner);
-        System.out.println("Hello! My name is Monday.");
-        System.out.println("How can I help you today?");
-        System.out.println("-------------------------");
-        System.out.println("Bye. Hope to see you again!");
-        System.out.println("------------------------");
+
+        String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+
+            System.out.println(line);
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(command);
+            System.out.println(line);
+        }
     }
 }
