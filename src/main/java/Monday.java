a22d1e615d6dbbc9fc2b7ffc5d85731394a32661
Jonathan Neo <jonathanntc04@gmail.com>
2026-08-20 14:17:02 +0800

    Changed the bot name from Duke to Monday

---
+++ src/main/java/Monday.java
@@ -0,0 +1,15 @@
+public class Monday {
+    public static void main(String[] args) {
+        String banner = " __  __   ___   _   _  ____      _    __   __\n"
+                + "|  \\/  | / _ \\ | \\ | ||  _ \\    / \\   \\ \\ / /\n"
+                + "| |\\/| || | | ||  \\| || | | |  / _ \\   \\ V / \n"
+                + "| |  | || |_| || |\\  || |_| | / ___ \\   | |  \n"
+                + "|_|  |_| \\___/ |_| \\_||____/ /_/   \\_\\  |_|  \n";
+        System.out.println(banner);
+        System.out.println("Hello! My name is Monday.");
+        System.out.println("How can I help you today?");
+        System.out.println("-------------------------");
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println("------------------------");
+    }
+}
